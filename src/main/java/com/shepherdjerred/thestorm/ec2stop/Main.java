b37d0c5a8317f762526c64fefb5300abc7336e4f
Jerred Shepherd <ShepherdJerred@gmail.com>
2020-06-07 12:04:26 -0700

    Init

---
+++ src/main/java/com/shepherdjerred/thestorm/ec2stop/Main.java
@@ -0,0 +1,10 @@
+package com.shepherdjerred.thestorm.ec2stop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class Main extends JavaPlugin {
+
+  @Override
+  public void onEnable() {}
+}
